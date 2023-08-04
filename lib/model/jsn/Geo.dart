ed42c43cfa61fc7cea8dd7080a0920124f84bc92
Bestin-Thomas <bestin.tcm@gmail.com>
2023-08-04 15:26:26 +0530

    Initial commit

---
+++ lib/model/jsn/Geo.dart
@@ -0,0 +1,22 @@
+
+
+class Geo {
+  Geo({
+      required this.lat,
+      required this.lng,});
+
+  Geo.fromJson(dynamic json) {
+    lat = json['lat'];
+    lng = json['lng'];
+  }
+  late  String lat;
+  late String lng;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['lat'] = lat;
+    map['lng'] = lng;
+    return map;
+  }
+
+}
---
+++ lib/providerservice.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:moc_api2/service/apiservice.dart';
+import 'model/employeedatabase.dart';
+import 'model/jsn/Employee.dart';
+
+
+class EmployeeProvider extends ChangeNotifier {
+  final EmployeeDatabase _employeeDatabase = EmployeeDatabase();
+  ApiService apiService=ApiService();
+  List<Employee> _employees = [];
+
+  List<Employee> get employees => _employees;
+
+  Future<void> fetchEmployees() async {
+    if (_employees.isEmpty) {
+      _employees = await _employeeDatabase.getEmployees();
+      notifyListeners();
+    }
+
+    if(_employees.length==0){
+      _employees=await apiService.getUsers();
+      print("updated");
+      notifyListeners();
+    }
+  }
+
+  List<Employee> searchEmployees(String query) {
+    if (query.isEmpty) {
+      return _employees;
+    } else {
+      return _employees.where((employee) =>
+      employee.name.toLowerCase().contains(query.toLowerCase()) ||
+          employee.email.toLowerCase().contains(query.toLowerCase())).toList();
+    }
+  }
+}
